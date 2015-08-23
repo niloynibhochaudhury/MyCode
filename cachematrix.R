053c7e13ba423524086dcbcb51d730e5d942b93b
niloynibhochaudhury <niloynibhochaudhury@yahoo.com>
2015-08-23 18:15:42 +0530

    This the Submission Code for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Purpose: This function creates a special "matrix" object that can cache its inverse
+#Input  : An invertible Matrix
+#OutPut : A list containing 
+#           function to set input values
+#           function to fetch the values of the input Matrix
+#           function to set the cache of the inverse Matrix
+#           function to fetch the cache of the inverse matrix
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(inputMat = matrix(numeric(0), nrow = 0, ncol = 0)) {
+  invMat <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
-
-
+  set <- function(cacheMat) {
+    inputMat <<- cacheMat
+    invMat <<- NULL
+  }
+  get <- function() inputMat
+  setinv <- function(inv) invMat <<- inv
+  getinv <- function() invMat
+  list(set = set, 
+       get = get, 
+       setinv = setinv,
+       getinv = getinv)
 }
 
+#Purpose: This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+#         If the inverse has already been calculated (and the matrix has not changed), 
+#         then the cachesolve should retrieve the inverse from the cache.
+#Input  : Special Matrix object returned by makeCacheMatrix function
+#Outout : Inverse of the given Matrix
 
-## Write a short comment describing this function
+cacheSolve <- function(inputMat, ...) {
+  invMat <- inputMat$getinv()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(invMat)) {
+    message("getting cached data")
+    return(invMat)
+  }
+
+  inputMat_fnc <- inputMat$get()
+  
+  invMat <- solve(inputMat_fnc, ...)
+
+  inputMat$setinv(invMat)
+
+  invMat
 }
